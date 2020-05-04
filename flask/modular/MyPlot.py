5328a5cdc1095d04a64ce3ccf7b9b3e4ea469915
Development <development@securtel.net>
2020-05-04 11:44:29 +0200

    Update MyPlot
    Add box and bar plots

--- flask/modular/MyPlot.py
+++ flask/modular/MyPlot.py
@@ -16,41 +16,31 @@ class MyPlot():
         return self.graphJSON
     
     def create_hist(self, df,  my_x_columns, my_y_columns):
-        title = "BAR Plot"
+        mtitle = "BAR Plot"
         self.my_x_columns = my_x_columns
         self.my_y_columns = my_y_columns
-        data = [
-            go.Bar(
-                x = df[''.join(my_x_columns)],
-                y = df[''.join(my_y_columns)]
-                
-            ),
-            go.Layout(
-                title=title,
-                xaxis=dict(
-                    title='X axis',
-                    type='log',
-                    autorange=True,
-                    titlefont=dict(
-                        family='Courier New, monospace',
-                        size=15,
-                        color='#7f7f7f'
-                    )
-                ),
-                yaxis=dict(
-                    title='Y axis',
-                    titlefont=dict(
-                        family='Courier New, monospace',
-                        size=15,
-                        color='#7f7f7f'
-                    )
-                ),
-                hovermode="closest"
+        fig = go.Figure()
+
+        for yvalue in my_y_columns:
+            fig.add_trace(go.Bar(
+                x=df[''.join(my_x_columns)],
+                y=df[''.join(yvalue)],
+                name="Trace "+''.join(yvalue),       # this sets its legend entry
+            ))
+
+        fig.update_layout(
+            title=mtitle,
+            xaxis_title=''.join(my_x_columns),
+            yaxis_title='/'.join(my_y_columns),
+            font=dict(
+                family="Courier New, monospace",
+                size=12,
+                color="#7f7f7f"
             )
-        ]
+        )        
 
 
-        self.graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+        self.graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     
     def create_scatter(self, df, my_x_columns, my_y_columns):
         mtitle = "SCATTER Plot"
@@ -58,7 +48,6 @@ class MyPlot():
         self.my_y_columns = my_y_columns
         
         fig = go.Figure()
-
 
         for yvalue in my_y_columns:
             fig.add_trace(go.Scatter(
@@ -68,14 +57,8 @@ class MyPlot():
                 mode = 'markers'
             ))
 
-        #fig.add_trace(go.Scatter(
-        #    x=[0, 1, 2, 3, 4, 5, 6, 7, 8],
-        #    y=[1, 0, 3, 2, 5, 4, 7, 6, 8],
-        #    name="Name of Trace 2"
-        #))
-
         fig.update_layout(
-            title="Plot Title",
+            title=mtitle,
             xaxis_title=''.join(my_x_columns),
             yaxis_title='/'.join(my_y_columns),
             font=dict(
@@ -87,9 +70,32 @@ class MyPlot():
 
         self.graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
-    def create_box(self, my_x_columns, my_y_columns):
-        pass
-    
+    def create_box(self, df, my_x_columns, my_y_columns):
+        mtitle = "BOX Plot"
+        self.my_x_columns = my_x_columns
+        self.my_y_columns = my_y_columns
+
+        fig = go.Figure()
+
+        for yvalue in my_y_columns:
+            fig.add_trace(go.Box(
+                x=df[''.join(my_x_columns)],
+                y=df[''.join(yvalue)],
+                name="Trace "+''.join(yvalue),       # this sets its legend entry
+            ))
+
+        fig.update_layout(
+            title=mtitle,
+            xaxis_title=''.join(my_x_columns),
+            yaxis_title='/'.join(my_y_columns),
+            font=dict(
+                family="Courier New, monospace",
+                size=12,
+                color="#7f7f7f"
+            )
+        )        
+        self.graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+        
     def testplot(self):
         N = 40
         x = np.linspace(0, 1, N)
